e464e4a84f9f000f8338386de06e290a8a0d79c2
Pip85 <78318926+Pip85@users.noreply.github.com>
2021-07-11 12:13:50 -0400

    Add map & tile layer

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+// Create map object
+var myMap = L.map("map", {
+    center: [39.8283, -98.5795],
+  zoom: 5
+});
+
+// Add tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
