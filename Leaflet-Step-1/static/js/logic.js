8eaa3ff91f83b36684b8b24c7f516bff818dc712
Pip85 <78318926+Pip85@users.noreply.github.com>
2021-07-14 18:54:22 -0400

    Add readme info and final code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,148 +1,7 @@
 //Create map object
-// var myMap = L.map("map", {
-//     center: [39.8283, -98.5795],
-//   zoom: 5
-// });
-
-//Create tile layer
-// var tiles = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "light-v10",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-// Store API path
-// var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-
-//_________________________________________________________________________________
-// Prior code to see if D3 connection to API is working
-// Connect to API through D3
-// d3.json(url).then(function(data) {
-//     L.geoJson(data, {
-//         style: function(feature) {
-//             return {
-//                 color: "green"
-//             };
-//         },
-//     }).addTo(myMap);
-// });
-//____________________________________________________________________________________
-
-// Connect to API through D3 and send to createMarkers function
-// d3.json(url).then(function(data) {
-    // createMarkers(data.features)
-//     createMarkers(data)
-// });
-
-// function getLatLng(coord) {
-//     var lat = coord[1];
-//     var lng = coord[0];
-//     var latlng = L.latLng(lat, lng);
-//     console.log (latlng);
-//     return latlng;
-// }
-
-// function markerSize(magnitude) {
-//     return magnitude/4; 
-// }
-
-
-// function createMarkers(quakes) {
-//     function features(feature, layer) {
-//         layer.bindPopup("<h5>" + feature.properties.place + "</h5><hr><h5>" + "Magnitude: " + feature.properties.mag + "</h5><h5>" + "Depth: " + feature.geometry.coordinates[2] + "</h5>");
-        
-//         var latlng = getLatLng(feature.geometry.coord);
-        
-//         L.circleMarker(latlng, {
-//             radius:  markerSize(feature.properties.mag),
-//             fillColor: "green",
-//             color: "green",
-//             fillOpactity: .5
-//         });
-
-        // pointToLayer: function (feature, coordinates) {
-        //     return L.circleMarker(latlng, geojsonMarkerOptions);
-        // }
-//     } 
-        
-//     L.geoJSON(quakes, {              
-//         onEachFeature: features
-//     }).addTo(myMap);
-// }
-
-
-//______________________________________________________________________________
-// TEST CODE ===================================================================
-//______________________________________________________________________________
-
-// Store API path
-// var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-
-// Connect to API through D3 and send to createMarkers function
-// d3.json(url).then(function(data) {
-//    var geojsonFeature = data;
-//     console.log(geojsonFeature)
-// });
-
-//Create map object
-// var myMap = L.map("map", {
-//     center: [39.8283, -98.5795],
-//   zoom: 5
-// });
-
-//Create tile layer
-// var tiles = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "light-v10",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-// Connect to API through D3 and send to createMarkers function
-// d3.json(url).then(function(data) {
-//     var earthquakeData = data;
-//     L.geoJSON(earthquakeData).addTo(myMap)
-    
-//     var coords = [];
-//     function features(feature, layer) {
-//         layer.bindPopup("<h5>" + feature.properties.place + "</h5><hr><h5>" + "Magnitude: " + feature.properties.mag + "</h5><h5>" + "Depth: " + feature.geometry.coordinates[2] + "</h5>");
-//         coords.push([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]);
-        
-//         var geojsonMarkerOptions = {
-
-//             fillColor: "green",
-//             color: "green",
-//             fillOpacity: .5,
-//             radius: 8
-//         };
-      
-//         pointToLayer: function (feature, latlng) {
-//             return L.circleMarker(coords, geojsonMarkerOptions);
-//         }.addTo(myMap)
-//     });
-//     };
-
-//     var earthquakeFeatures = L.geoJSON(earthquakeData, {
-//         onEachFeature: features
-//         pointToLayer: 
-//     });
-    
-//     earthquakeFeatures.addTo(myMap);
-// })
-
-//______________________________________________________________________________
-// TEST CODE ===================================================================
-//______________________________________________________________________________
-
-//Create map object
 var myMap = L.map("map", {
     center: [39.8283, -98.5795],
-  zoom: 5
+  zoom: 4
 });
 
 //Create tile layer
@@ -155,78 +14,34 @@ var tiles = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/
   accessToken: API_KEY
 }).addTo(myMap);
 
-
-
-// Create function to determine marker size based on magnitude
-
-
-
-
-// Store API path
+// Store URL path
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-// Connect to API through D3 and send to createMarkers function
+// Connect to geoJSON through D3
 d3.json(url).then(function(data) {
 
+    // Create function to gather lat/lng from coordinates
     function getLatLng(coord) {
             var lat = coord[1];
             var lng = coord[0];
-            var latlng = L.latLng(lat, lng);
-            var depth = coord[2];
-            console.log (latlng);
+            var latlng = L.latLng(lat, lng);           
             return latlng, depth;
     }    
-    
-    function getColor(colorDepth) {
-        var color = "";
-        if(colorDepth < 10) {
-            color = "blue"
-        }
-        else if (colorDepth <30 && colorDepth >= 10) {
-            color ="green"
-        }
-        else if (colorDepth <50 && colorDepth >= 30) {
-            color ="red"
-        }
-        else if (colorDepth <70 && colorDepth >= 50) {
-            color ="yellow"
-        }
-        else if (colorDepth <90 && colorDepth >= 70) {
-            color ="purple"
-        }        
-        else {
-            color ="orange"
-        }
-        return color;
-    }
-        
-    // function markerSize(magnitude) {
-    //     var mag = magnitude.properties.mag/4;        
-    //     return mag;
-    // }
+   
+    // Create function to change circle marker color based on depth
+    function getColor(d) {
+        return d > 90 ? '#DE3163':
+               d > 70 ? '#20A387FF':
+               d > 50 ? '#ff8700':
+               d > 30 ? '#55A630':
+               d > 10 ? '#FDE725FF':               
+                        '#404788FF';
+    }    
 
-        
-    // var geojsonMarkerOptions = {
-    //     fillColor: "green",
-    //     color: "green",
-    //     fillOpacity: .5              
-    // };
-
-    // console.log(mag)
-
+    // Use geoJSON data to populate the map
     L.geoJSON(data, {
-        // geojsonLayer = (data, {
-        // style: function(feature) {
-        //     var color = "";
-        //     if depth < 10) {
-
-        //     }
-        //     return {
-        //         fillColor: "green",
-        //         color: "green"         
-        //     };
-        // },
      
+        // Create circle markers on map and size based on magnitude
         pointToLayer: function (feature, latlng) {          
             return new L.circleMarker(latlng, {
                 radius: feature.properties.mag*2.5,
@@ -237,21 +52,30 @@ d3.json(url).then(function(data) {
             });
         },
 
+        // Add popup for each marker with earthquake details
         onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h5>" + feature.properties.place + "</h5><hr><h5>" + "Magnitude: " + feature.properties.mag + "</h5><h5>" + "Depth: " + feature.geometry.coordinates[2] + "</h5>"); 
+            layer.bindPopup("<h5>" + feature.properties.place + "</h5><hr><h5>" + "Magnitude: " + feature.properties.mag + 
+            "</h5><h5>" + "Depth: " + feature.geometry.coordinates[2] + "</h5>"); 
+        },
+    }).addTo(myMap);
+       
+    // Create legend for depth colors 
+    var legend = L.control({position:  "bottomright"});
+    legend.onAdd = function(myMap) {
+        var div = L.DomUtil.create("div", "info legend");
+        limits = [-20, 10, 30, 50, 70, 90],
+        labels = [];
+        
+        for (var i = 0; i < limits.length; i++) {
+            div.innerHTML +=
+            labels.push(
+            '<i class = circle style="background:' + getColor(limits[i] + 1) + '"></i> ' +
+            limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1]  : '+'));
         }
-    }).addTo(myMap)
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+    legend.addTo(myMap);
+  });
 
-    // map.addLayer(geojsonLayer);
-});    
-        // style: function (feature) {
-    //         return {
-    //             fillColor: "green",
-    //             color: "green",
-    //             fillOpacity: .5,
-    //             radius: 8
-    //         };            
-    //     },
-    //     onEachFeature: function(feature, layer) {
-    //         layer.bindPopup("<h5>" + feature.properties.place + "</h5><hr><h5>" + "Magnitude: " + feature.properties.mag + "</h5><h5>" + "Depth: " + feature.geometry.coordinates[2] + "</h5>"); 
-    // });
+      
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # leaflet-challenge
-Use leaflet to map USGA Earthquake data for 7 days in July 2021.
+Challenge:
+The challenge was to use leaflet.js to visualize earthquakes across the world.
+
+Data:
+The map of earthquake sites was based on data from the US Geological Survey site below:
+http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+The data included earthquake information such as latitude and longitude coordinates, depth of earthquake, magnitude of earthquake and text location over the last seven days(as of 7/14/21).  The data was presented in a geoJSON format.
+
+
+Code:
+Initial code included setting up a map using leaflet and importing the tile layers for the map.  Then a data connection to the API was established using D3.JSON.  After connecting to the data, the latitude and longitude coordinates were gathered to mark the earthquake locations.
+
+Using leaflet geoJSON, a map layer was created to show locations with a circle marker.  The marker size is determined by the earthquake magnitude.  This was done by adjusting the radius of each circle to correspond with the earthquake magnitude in each location.
+
+Marker colors were also added by assigning colors to each marker based on a locations earthquake depth.
+
+Next, popups were added to each marker showing the earthquake location, depth and magnitude.  The popups are visible by clicking on a marker.
+
+The final piece of the code is to add the legend of colors for each depth scale.  This was added to the bottom right of the map.
+
+The data pulled for this challenge showed relatively small size and depth throughout the United States as compared to other parts of the world, particularly South America's west coast.  The glaring exception to that is Alaska, which has more activity and variety of depth than even the well-know earthquake centric California.
+
+Take a look at the earthquake activity in July 2021!
+
+![Earthquakes North America](images/Earthquakes 07-14-21 North America.png)
